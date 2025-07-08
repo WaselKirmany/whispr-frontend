067d403b0d83e3773744322e2e5938af98f12269
Sayed Wasel Kirmany <64572797+WaselKirmany@users.noreply.github.com>
2025-07-08 22:29:12 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://whispr-backend.vercel.app/api",
   withCredentials: true, // This is important for sending cookies
 });
